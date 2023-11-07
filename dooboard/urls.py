9e46178cd2a878a4859149dd24d44f51286b1542
jjang36524 <jjang36524@naver.com>
2023-11-07 10:09:24 +0900

    added more stuff

--- dooboard/urls.py
+++ dooboard/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name='doobo'
 urlpatterns = [
     path('',views.index,name='index'),
     path('<int:player_id>',views.detail,name='detail')
